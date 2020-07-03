ae05f6f707486d0358ea04c6c0542fa10f971623
MassDo <dorian_massoulier@hotmail.com>
2020-07-03 12:42:43 +0200

    Création du projet avec cookiecutter-django

---
+++ massdo_api/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from massdo_api.users.models import User
+from massdo_api.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
